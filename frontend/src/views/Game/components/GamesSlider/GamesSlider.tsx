6f6129e5d7d26d5cd03f2117dd6db18b7db38368
aemanskova <ae.manskova@mail.ru>
2024-10-15 00:21:04 +0300

    add slider

--- frontend/src/views/Game/components/GamesSlider/GamesSlider.tsx
+++ frontend/src/views/Game/components/GamesSlider/GamesSlider.tsx
@@ -1,38 +1,83 @@
-import React from "react";
-const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-  };
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-  import Slider from 'react-slick'
+import slide1 from '@assets/slider-picture-1.png'
+import slide2 from '@assets/slider-picture-2.png'
+import slide3 from '@assets/slider-picture-3.png'
+import slide4 from '@assets/slider-picture-4.png'
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/thumbs';
+import '../GamesSlider/GamesSlider.scss'
+
+// import required modules
+import { FreeMode, Navigation, Thumbs, Pagination } from 'swiper/modules';
+import { relative } from 'path';
+
 const GameSlider: React.FC = () => {
+
+  const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
   return (
-    <Slider {...settings}>
-    <div>
-      <h3>SLIDE 1</h3>
+    <div style={{position: 'relative', maxWidth: '750px'}}>
+    <Swiper
+        spaceBetween={10}
+        pagination={{
+          type: 'progressbar',
+        }}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Thumbs,
+          
+      Pagination
+    ]}
+        className="mySwiper2"
+      >
+        <SwiperSlide>
+          <img src={slide1} />
+        </SwiperSlide>
+        <SwiperSlide>
+        <img src={slide2} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} />
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        pagination={{
+          el: '.swiper-pagination',
+          type: 'progressbar',
+          enabled: false,
+          lockClass: "lala"
+        }}
+        modules={[FreeMode, Thumbs, Pagination]}
+        className="mySwiper"
+      >
+         <SwiperSlide>
+          <img src={slide1} />
+        </SwiperSlide>
+        <SwiperSlide>
+        <img src={slide2} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} />
+        </SwiperSlide>
+      </Swiper>
     </div>
-    <div>
-      <h3>SLIDE 2</h3>
-    </div>
-    <div>
-      <h3>SLIDE 3</h3>
-    </div>
-    <div>
-      <h3>SLIDE 4</h3>
-    </div>
-    <div>
-      <h3>SLIDE 5</h3>
-    </div>
-    <div>
-      <h3>SLIDE 6</h3>
-    </div>
-  </Slider>
-  );
+  )
 };
 
 export default GameSlider;
