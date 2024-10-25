bd770cb04cf2bbd2a0f8fc982dcd5fba83a71ac2
aemanskova <ae.manskova@mail.ru>
2024-10-25 22:38:25 +0300

    continue refactor

--- frontend/src/views/PostForm/AsideMenu/Sidebar.tsx
+++ frontend/src/views/PostForm/AsideMenu/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
-import './Sidebar.scss';
+import styles from './Sidebar.module.scss';
 import Star from '../../../assets/decorate-star-mini.svg';
 
 // Интерфейс для пропсов компонента Sidebar
@@ -13,13 +13,13 @@ export const Sidebar: React.FC<SidebarProps> = ({ activeSection, setActiveSectio
     const sections = ['Основная информация', 'Изображения продукта', 'Файлы игры'];
 
     return (
-        <div className='post-form__sidebar'>
-            <img className="sidebar__decoration" src={Star} alt="Звезда" />
+        <div className={styles['post-form__sidebar']}>
+            <img className={styles["sidebar__decoration"]} src={Star} alt="Звезда" />
             {sections.map((section) => (
                 <button
                     key={section}
                     onClick={() => setActiveSection(section)}
-                    className={activeSection === section ? 'sidebar__btn_active' : 'sidebar__btn'}
+                    className={activeSection === section ? styles['sidebar__btn_active'] : styles['sidebar__btn']}
                 >
                     {section}
                 </button>
