afb0e528c491487cf7dd4aa2245c5e077e8f0609
stpavlenko <stpavlenko@mail.ru>
2024-11-16 19:45:25 +0300

    add formatter

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ text }) => {
     return (
         <button className={styles['styled-button']}>
-            <img //fasdfsadf not empty asdfasdfa
+            <img //fasdfsadf not empty asdfasdfa fasdfasdfsafsad
                 src={arrow}
                 alt="Arrow"
                 className={styles['styled-button__arrow']}
