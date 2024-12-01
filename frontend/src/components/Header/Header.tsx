beebef699e389d3cbf5e320ebef6cf1d2c715129
Akulov Kirill <113677480+KirAuki@users.noreply.github.com>
2024-12-01 21:29:38 +0300

    убрал лишние элементы с header, поставил базовую тему dark

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "@assets/logo.svg";
 import Search from "./Search/Search";
-import ThemeSwitcher from "./ThemeSwitcher/ThemeSwitcher";
 import MenuButton from "./MenuButton/MenuButton";
 import styles from "./Header.module.scss";
 
@@ -20,35 +19,16 @@ const Header: React.FC<HeaderProps> = ({ openMenu, showHeader }) => {
             }`}
         >
             <div className={styles.header__wrapper}>
-                <ul className={styles.header__details}>
-                    <li className={styles.header__item}>
-                        <Link to="/" className={styles.header__link}>
-                            <img
-                                src={logo}
-                                alt="PolyGames Logo Image"
-                                className={styles.header__logo}
-                            />
-                        </Link>
-                        <Search />
-                    </li>
-                    <li className={styles.header__item}>
-                        <ThemeSwitcher />
-                        <div className={styles.header__auth}>
-                            <Link to="/auth" className={styles.profile__link}>
-                                <p className={styles.profile__login}>Вход</p>
-                            </Link>
-                            <Link
-                                to="/register"
-                                className={styles.profile__link}
-                            >
-                                <p className={styles.profile__register}>
-                                    Регистрация
-                                </p>
-                            </Link>
-                        </div>
-                        <MenuButton clickButton={openMenu} />
-                    </li>
-                </ul>
+                <div className={styles.header__details}>
+                    <Link to="/" className={styles.header__link}>
+                        <img
+                            src={logo}
+                            alt="PolyGames Logo Image"
+                            className={styles.header__logo}
+                        />
+                    </Link>
+                    <MenuButton clickButton={openMenu} />
+                </div>
             </div>
         </header>
     );
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,8 @@ import App from './components/App'
 import { Provider } from 'react-redux'
 import store from './store/store'
 
+
+document.documentElement.setAttribute("data-theme", "dark");
 render(
     <Provider store={store}>
         <App/>
